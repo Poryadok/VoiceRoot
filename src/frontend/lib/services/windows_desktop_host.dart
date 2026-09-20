84839c229e3aadd113d73216d3197484685f0020
Poryadok <ChronosVellior@yandex.ru>
2026-09-20 14:39:37 +0300

    Harden Windows tray voice lifecycle

--- src/frontend/lib/services/windows_desktop_host.dart
+++ src/frontend/lib/services/windows_desktop_host.dart
@@ -9,6 +9,7 @@ abstract class WindowsDesktopHost {
   void setListener(WindowsDesktopHostListener? listener);
 
   Future<void> setTrayState({
+    required bool voiceActive,
     required bool muted,
     required bool deafened,
     required String muteLabel,
@@ -95,6 +96,7 @@ class MethodChannelWindowsDesktopHost implements WindowsDesktopHost {
 
   @override
   Future<void> setTrayState({
+    required bool voiceActive,
     required bool muted,
     required bool deafened,
     required String muteLabel,
@@ -105,6 +107,7 @@ class MethodChannelWindowsDesktopHost implements WindowsDesktopHost {
   }) async {
     await _channel.invokeMethod<void>('setTrayState', {
       'muted': muted,
+      'voiceActive': voiceActive,
       'deafened': deafened,
       'muteLabel': muteLabel,
       'unmuteLabel': unmuteLabel,
@@ -152,6 +155,9 @@ class RecordingWindowsDesktopHost implements WindowsDesktopHost {
   int quitCalls = 0;
   int registerHotkeyCalls = 0;
   int? lastVkCode;
+  bool? lastVoiceActive;
+  bool? lastMuted;
+  bool? lastDeafened;
 
   @override
   void setListener(WindowsDesktopHostListener? next) {
@@ -178,6 +184,7 @@ class RecordingWindowsDesktopHost implements WindowsDesktopHost {
 
   @override
   Future<void> setTrayState({
+    required bool voiceActive,
     required bool muted,
     required bool deafened,
     required String muteLabel,
@@ -185,7 +192,11 @@ class RecordingWindowsDesktopHost implements WindowsDesktopHost {
     required String deafenLabel,
     required String undeafenLabel,
     required String quitLabel,
-  }) async {}
+  }) async {
+    lastVoiceActive = voiceActive;
+    lastMuted = muted;
+    lastDeafened = deafened;
+  }
 
   @override
   Future<void> registerPttHotkey({
@@ -221,6 +232,7 @@ class NoopWindowsDesktopHost implements WindowsDesktopHost {
 
   @override
   Future<void> setTrayState({
+    required bool voiceActive,
     required bool muted,
     required bool deafened,
     required String muteLabel,
